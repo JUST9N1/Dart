03049ce1d744de609c0698f226d6fe2ccaedb048
Sujan Gautam <susangautam062@gmail.com>
2024-05-13 20:30:12 +0545

    added calculator screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tempflutter/screen/custom_widget_field.dart';
-import 'package:tempflutter/screen/flexible_expanded_screen.dart';
-import 'package:tempflutter/screen/flutter_layout_screen.dart';
 import 'package:tempflutter/screen/list_tile.dart';
-import 'package:tempflutter/screen/output_screen.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -17,7 +13,7 @@ class _AppState extends State<App> {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FlexibleExpandedScreen(),
+      home: ListTileScreen(),
     );
   }
 }
